b39281c4ddda6ecd0d87f7d1a0436136fbbcebcd
angkrz89 <angkrz89@users.noreply.github.com>
2021-06-26 12:34:21 +0000

    Test COMBAT

--- combat.py
+++ combat.py
@@ -38,14 +38,14 @@ class CombatShopping(unittest.TestCase):
         # 1. Zaakceptuj "ciasteczka"
         accept_btn = driver.find_element_by_id('cookie-accept')
         accept_btn.click()
-        
+
         # 2. Wyszukaj "teleskop"
         search_input = driver.find_element_by_id('search')
         search_input.send_keys(valid_search)
-        
+
         # 3. Wybierz model "Teleskop Celestron Perceptor Travel 50 mm"
         driver.find_element_by_xpath('//a[@title="Teleskop Celestron Perceptor Travel 50 mm"]').click()
-        
+
         # 4. Dodaj go do koszyka
         add_btn = driver.find_element_by_xpath('//button[@title="Dodaj do koszyka"]')
         add_btn.click()
@@ -56,72 +56,71 @@ class CombatShopping(unittest.TestCase):
 	# 5. Wyszukaj "wiatrówka"
         search_input = driver.find_element_by_id('search')
         search_input.send_keys(valid_search2)
-        
+
         # 6. Wybierz model "Karabinek Hammerli Black Force 800 Combo"
         driver.find_element_by_xpath('//a[@title="Wiatrówka Karabinek Hammerli Black Force 800 Combo..."]').click()
-        
+
         # 7. Dodaj go do koszyka
         add_btn = driver.find_element_by_xpath('//button[@title="Dodaj do koszyka"]')
         add_btn.click()
 
-	# Poczekaj 5 sekund
+	    # Poczekaj 5 sekund
         sleep(5)
 
-	# 8. Przejdź do koszyka
+	    # 8. Przejdź do koszyka
         basket_btn = driver.find_element_by_xpath('//a[@href="https://www.combat.pl/checkout/cart/"]')
         basket_btn.click()
-        
+
         # 9. Przejdź do kasy
         checkout_btn = driver.find_element_by_xpath('//button[@title="Przejdź do kasy"]').click()
-        
-	# Poczekaj 10 sekund
+
+	    # Poczekaj 10 sekund
         sleep(10)
 
-	# 10. Wpisz nieprawidłowy adres e-mail (brak znaku '@')
+	    # 10. Wpisz nieprawidłowy adres e-mail (brak znaku '@')
         email_input = driver.find_element_by_id('customer-email')
         email_input.click()
         email_input.send_keys(invalid_email)
-        
+
         # 11. Wpisz imię
         imie_input = driver.find_element_by_name('firstname')
         imie_input.send_keys(valid_firstname)
-        
+
         # 12. Wpisz nazwisko
         nazwisko_input = driver.find_element_by_name('lastname')
         nazwisko_input.send_keys(valid_lastname)
-        
+
         # 13. Wpisz ulicę
         street_input = driver.find_element_by_name('street[0]')
         street_input.send_keys(valid_street)
-        
+
         # 14. Wpisz numer domu/lokalu
         street_number_input = driver.find_element_by_name('street[1]')
         street_number_input.send_keys(valid_street_number)
-        
+
         # 15. Wpisz miasto
         city_input = driver.find_element_by_name('city')
         city_input.send_keys(valid_city)
-        
+
         # 16. Wpisz kod pocztowy
         street_number_input = driver.find_element_by_name('postcode')
         street_number_input.send_keys(valid_postcode)
-        
+
         # 17. Wpisz numer telefonu
         phone_number_input = driver.find_element_by_name('telephone')
         phone_number_input.send_keys(valid_phone_number)
-        
+
         # 18. Wybierz sposób dostawy: Kurier - płatność przy odbiorze
         shipping_btn = driver.find_element_by_id('label_method_flatrate_flatrate').click()
-        
-        # scroll page down
 
+        # Scroll page down
         html = driver.find_element_by_tag_name('html')
         html.send_keys(Keys.END)
-       
+
         # 19. Przejdź do płatności
         payment_btn = driver.find_element_by_xpath('//button[@data-role="opc-continue"]')
 
-	# Wyszukuje wszystkie błędy
+	    # Wyszukuje wszystkie błędy
         error_messages = driver.find_elements_by_css_selector('div#customer-email-error')
         # Tworzę listę WIDOCZNYCH błędów
         visible_error_notices = list() # Pusta lista
@@ -130,15 +129,15 @@ class CombatShopping(unittest.TestCase):
             if error.is_displayed():
             	# Dodajemy ten komunikat do listy WIDOCZNYCH
                 visible_error_notices.append(error)
-              
-        # Sprawdzam, czy ta lista WIDOCNYCH komunikatów zawiera tylko jeden błąd      
+
+        # Sprawdzam, czy ta lista WIDOCNYCH komunikatów zawiera tylko jeden błąd
         assert len(visible_error_notices) == 1, "Liczba widocznych komunikatów nie zgadza się!"
         # Z wykorzystaniem unittesta
         self.assertEqual(len(visible_error_notices), 1, msg="Liczba widocznych komunikatów nie zgadza się!")
         # Sprawdzam treść błędu
         self.assertEqual(visible_error_notices[0].text, "Prosimy wprowadzić poprawny adres email (np.: imie@nazwisko.com).")
-	
-	# Poczekaj 3 sekundy
+
+    	# Poczekaj 3 sekundy
         sleep(3)
 
     def tearDown(self):
